60bce7db949feff90a545b8843346f7566bd41f3
Sergey Golovkin <netcat09@gmail.com>
2016-12-25 20:32:02 +0200

    All GitHub Users

--- controllers/getusers.js
+++ controllers/getusers.js
@@ -6,25 +6,35 @@ app.controller ('getUsersInfo', function ($scope, API){
     $scope.url = 'https://api.github.com/users';
     API.getInfo($scope.url).then(function (users) {
             $scope.users = users;
+            $scope.pPageSince = $scope.users[0].id;
+            console.log($scope.pPageSince);
+            $scope.nPageSince = $scope.users[$scope.users.length - 1].id;
+            console.log($scope.nPageSince);
+
         });
 
     $scope.nPage = function () {
-        $scope.since = $scope.since + 30;
+        $scope.since = $scope.nPageSince;
         $scope.url = 'https://api.github.com/users?since=' + $scope.since;
         console.log($scope.since);
         API.getInfo($scope.url).then(function (users) {
             $scope.users = users;
+            $scope.pPageSince = $scope.users[0].id;
+            console.log($scope.pPageSince);
+            $scope.nPageSince = $scope.users[$scope.users.length - 1].id;
+            console.log($scope.nPageSince);
         });
     };
     $scope.pPage = function () {
-        $scope.since = $scope.since - 30;
-        if ($scope.since < 0){
-            $scope.since = 0
-        }
+        $scope.since = $scope.pPageSince;
         $scope.url = 'https://api.github.com/users?since=' + $scope.since;
         console.log($scope.since);
         API.getInfo($scope.url).then(function (users) {
             $scope.users = users;
+            $scope.pPageSince = $scope.users[0].id;
+            console.log($scope.pPageSince);
+            $scope.nPageSince = $scope.users[$scope.users.length - 1].id;
+            console.log($scope.nPageSince);
         });
     }
 
